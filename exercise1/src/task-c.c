3e523d6774e2b3ab7230a6beb071151518573cf6
Eric Thörn <eric.m.thorn@gmail.com>
2020-09-08 15:21:21 +0200

    andre utkast Task C - histogram

--- exercise1/src/task-c.c
+++ exercise1/src/task-c.c
@@ -1,17 +1,41 @@
 #include <stdio.h>
 #include <sched.h>
 #include <time.h>
+#include <x86intrin.h>
+#include <string.h>
 
-int main()
-{
+// int main()
+// {
+//   struct timespec now, after;
+
+//   clock_gettime(CLOCK_MONOTONIC, &now);
+//   sched_yield();
+//   clock_gettime(CLOCK_MONOTONIC, &after);
+
+//   printf("%ldns\n", (after.tv_nsec - now.tv_nsec));
+//   return 0;
+// }
+
+
+int main() {
   struct timespec now, after;
-  clock_gettime(CLOCK_MONOTONIC, &now);
+  int ns_max = 50;
+  int histogram[ns_max];
+  memset(histogram, 0, sizeof(int)*ns_max);
 
-  //clock_t t1, t2;
-  clock_gettime(CLOCK_MONOTONIC, &now);
-  sched_yield();
-  clock_gettime(CLOCK_MONOTONIC, &after);
-  //printf("%Lfs\n", (long double)(t2 - t1)/CLOCKS_PER_SEC);
-  printf("%ldns\n", (after.tv_nsec - now.tv_nsec));
+  for (int i = 0; i < 10*1000*1000; i++) {
+    clock_gettime(CLOCK_MONOTONIC, &now);
+    sched_yield();
+    clock_gettime(CLOCK_MONOTONIC, &after);
+    int ns = (after.tv_nsec - now.tv_nsec);
+    if (ns >= 0 && ns < ns_max) {
+      histogram[ns]++;
+    }
+  }
+
+  for (int i = 0; i < ns_max; i++) {
+    printf("%d\n", histogram[i]);
+  }
+  
   return 0;
 }
